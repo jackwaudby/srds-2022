359cd033d86fe9d3cf63f583bfec396879330805
Jack Waudby <jack.waudby@neo4j.com>
2023-02-22 10:17:49 +0000

    add plot script

--- code/sim-long-running-txns/scripts/plot.R
+++ code/sim-long-running-txns/scripts/plot.R
@@ -3,7 +3,7 @@ suppressPackageStartupMessages(library(readr))
 
 raw = read_csv(file = "scripts/results.csv",col_names = TRUE)
 
-p1 = ggplot(data = raw, aes(x = k, y = completedJobPs)) +
+p1 = ggplot(data = raw, aes(x = k, y = completedJobPs/2)) +
   geom_line() + xlab("proportion of long running transactions") + ylab("completed txn/s") + theme_bw()
 p1
 ggsave("test.png", p1, width = 8, height = 6, device = "png")
