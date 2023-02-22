7934ef493ae9b23ab1ec22454d7340e70320e202
Jack Waudby <jack.waudby@neo4j.com>
2023-02-22 10:30:53 +0000

    cleanup files

--- code/sim-decentralised/build-image.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-mvn clean install
-
-sudo docker build -t sim-infinite-queue-epoch:latest .
--- code/sim-decentralised/scripts/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp sim.jar Main -n 64 -b 1.7 -c 1.7 -mu 1 -xi 675000 -eta 1800000 -m 10 -af true -a "$1" -p "$2" -d "$3"
