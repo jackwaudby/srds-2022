e009e18d43148dd6ca59a1a2075cb378b5561ad0
Jack Waudby <jack.waudby@neo4j.com>
2023-02-24 10:05:31 +0000

    edit run scripts

--- code/sim-long-running-txns/scripts/run.sh
+++ code/sim-long-running-txns/scripts/run.sh
@@ -2,5 +2,5 @@
 
 for m in 0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1
 do
-    java -cp ../target/sim-long-running-txns-1.0-SNAPSHOT.jar Main --duration 10 -pl "$m"
+    java -cp ../target/sim-long-running-txns-1.0-SNAPSHOT.jar Main --duration 100 -pl "$m"
 done
